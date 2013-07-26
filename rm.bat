ac46548bbc9cefb5a2663d43866d24942f9df84c
柳祚鹏 <liuzp19890810@gmai.com>
2013-07-26 10:56:55 -0700

    first commit dev branch

--- rm.bat
+++ rm.bat
@@ -2,11 +2,14 @@
 
 set CURPATH=C:\liuzp\dev\OpenCLProject
 
+cd %CURPATH%
 del /S /F /Q *.ncb *sdf
 rd /S /Q %CURPATH%\Debug 
 
 cd %CURPATH%\Test 
 rd /S /Q Debug
+del /S /F /Q *.ncb *sdf
 
 cd %CURPATH%\MatrixVectorMultiply
 rd /S /Q Debug
+del /S /F /Q *.ncb *sdf
